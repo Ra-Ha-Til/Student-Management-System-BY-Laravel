09bcc161c8bc1ec1001eebee88ac50bb9e5f9971
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-01 18:27:01 +0600

    index updated

--- resources/views/Student/index.blade.php
+++ resources/views/Student/index.blade.php
@@ -1,55 +1,73 @@
-@extends('students.layout')
+@extends('layout')
 
 @section('content')
     <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Student List</h2>
-            </div>
-            <div class="pull-right mb-2">
-                <a class="btn btn-success" href="{{ url('/student/create') }}"> <i class="fa fa-plus"></i> Add New
-                    Student</a>
-            </div>
-        </div>
-    </div>
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Student List</h2>
+                </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success mt-2">
-            {{ $message }}
-        </div>
-    @endif
+                <div class="card-body">
+                    <a href="{{ url('/student/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+                        <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                    </a>
+                    <br><br>
 
-    <table class="table table-bordered mt-3">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Name</th>
-                <th>Address</th>
-                <th>Mobile</th>
-                <th width="220px">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($students as $item)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->address }}</td>
-                    <td>{{ $item->mobile }}</td>
-                    <td>
-                        <a class="btn btn-info btn-sm" href="{{ url('/student/' . $item->id) }}" title="View">View</a>
-                        <a class="btn btn-primary btn-sm" href="{{ url('/student/' . $item->id . '/edit') }}"
-                            title="Edit">Edit</a>
-                        <form action="{{ url('/student/' . $item->id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Address</th>
+                                    <th>Mobile</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($students as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->address }}</td>
+                                        <td>{{ $item->mobile }}</td>
+                                        <td>
+                                            <a href="{{ url('/student/' . $item->id) }}" title="View Student">
+                                                <button class="btn btn-info btn-sm">
+                                                    <i class="fa fa-eye" aria-hidden="true"></i> View
+                                                </button>
+                                            </a>
+
+                                            <a href="{{ url('/student/' . $item->id . '/edit') }}" title="Edit Student">
+                                                <button class="btn btn-primary btn-sm">
+                                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                                                </button>
+                                            </a>
+
+                                            <form method="POST" action="{{ url('/student/' . $item->id) }}"
+                                                accept-charset="UTF-8" style="display:inline"
+                                                onsubmit="return confirm('Are you sure you want to delete this student?');">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Student">
+                                                    <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                                @if ($students->isEmpty())
+                                    <tr>
+                                        <td colspan="5" class="text-center">No students found.</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div> <!-- /.table-responsive -->
+                </div> <!-- /.card-body -->
+            </div> <!-- /.card -->
+        </div> <!-- /.col-md-9 -->
+    </div> <!-- /.row -->
 @endsection
