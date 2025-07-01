f836a9c967a148f4f8c0b2abb2ca1281d2c745c0
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-01 20:23:08 +0600

    create.blade added

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,14 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('students.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+@section('content')
+    <div class="card">
+        <div class="card-header">Students Page</div>
+        <div class="card-body">
+            <form action="{{ url('student') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" name="address" id="address" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="mobile">Mobile</label>
+                    <input type="text" name="mobile" id="mobile" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Save" class="btn btn-success">
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
