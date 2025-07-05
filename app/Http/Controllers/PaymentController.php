8ac618c45d4bfa29c18411204972b325eca63a36
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-05 22:46:15 +0600

    payments resource created

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Payment;
 
 class PaymentController extends Controller
 {
@@ -11,7 +12,8 @@ class PaymentController extends Controller
      */
     public function index()
     {
-        //
+        $payments = Payment::all();  // Make sure it fetches all fields
+        return view('payments.index', compact('payments'));
     }
 
     /**
@@ -19,7 +21,8 @@ class PaymentController extends Controller
      */
     public function create()
     {
-        //
+        $payments = Payment::pluck('enroll_no', 'id');
+        return view('payments.create', compact('payments'));
     }
 
     /**
@@ -27,7 +30,11 @@ class PaymentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+
+        $input = $request->all();
+        Payment::create($input);
+        return redirect('payments')->with('flash_message', 'Payment Added!');
     }
 
     /**
@@ -35,7 +42,8 @@ class PaymentController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $payments = Payment::find($id);
+        return view('payments.show')->with('payments', $payments);
     }
 
     /**
@@ -43,7 +51,8 @@ class PaymentController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $payments = Payment::find($id);
+        return view('payments.edit')->with('payments', $payments);
     }
 
     /**
@@ -51,7 +60,10 @@ class PaymentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $payments = Payment::findOrFail($id);
+        $input = $request->all(); // get all fields
+        $payments->update($input);
+        return redirect('payments')->with('flash_message', 'Batch Updated!');
     }
 
     /**
@@ -59,6 +71,7 @@ class PaymentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        Payment::destroy($id);
+        return redirect('payments')->with('flash_message', 'Batch Deleted!');
     }
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace App\Models;
 
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $table = 'payments';
+    protected $primaryKey = 'id';
+    protected $fillable = ['enrollment_id', 'paid_date', 'amount'];
+    use HasFactory;
+
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
 }
